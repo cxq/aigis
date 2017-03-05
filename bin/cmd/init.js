a2dfb77a1f64686e9aa7edf4eb4c25b1fad33a26
Xiu qiang <chuxiuqiang@gmail.com>
2017-03-05 22:33:04 +0100

    move components templates

--- bin/cmd/init.js
+++ bin/cmd/init.js
@@ -2,7 +2,7 @@ exports.builder = {
   'e': {
     alias: 'engine',
     describe: 'choose template engine [ejs, jade, hbs]',
-    default: 'ejs'
+    default: 'hbs'
   }
 };
 
@@ -10,36 +10,31 @@ exports.handler = function init(argv) {
   var path = require('path');
   var fs = require('fs-extra');
   var cwd = path.resolve();
-  var exampleDir = path.join(__dirname, '../../examples');
+  var rootDir = path.join(__dirname, '../..');
   var colors = require('colors/safe');
   
   var assets = [
     {
-      dest: 'styleguide.yml',
-      src: 'default_config.yml'
+      src: 'default_config.yml',
+      dest: 'styleguide.yml'
     },
     {
-      src: 'assets',
-      dest: 'assets'
-    },
-    {
-      src: 'template_' + argv.engine,
-      dest: 'template_' + argv.engine
+      src: 'styleguide-templates',//Copy the core templates
+      dest: 'styleguide-templates'
     }
   ];
-
+  
   console.log(colors.blue('Created the following files and directories:'));
-  assets.forEach(function(asset) {
+  
+  assets.forEach(function (asset) {
     var src = asset.src, dest = asset.dest;
-    var _path = path.join(exampleDir, src);
+    var _path = path.join(rootDir, src);
     var _dest = path.join(cwd, dest);
-
-    switch(src) {
+    
+    switch (src) {
       case 'default_config.yml':
         var file = fs.readFileSync(_path, 'utf-8');
-        file += 'template_dir: ./template_' + argv.engine + '\n';
-        file += 'template_engine: ' + argv.engine + '\n';
-
+        
         try {
           var config = fs.readFileSync(_dest);
           console.warn(colors.yellow('Cowardly refusing to overwrite existing: styleguide.xml'));
@@ -51,9 +46,10 @@ exports.handler = function init(argv) {
         break;
       default:
         console.log(' ', path.relative(cwd, _dest));
+        
         fs.copySync(_path, _dest, {
-          filter: function(file) {
-            console.log(colors.blue(' ', path.relative(exampleDir, file)));
+          filter: function (file) {
+            console.log(colors.blue(' ', path.relative(rootDir, file)));
             return true;
           }
         });
